4eb9d667e59e47ef1d279afec60baaf703577e2f
wayou <liuwayong@gmail.com>
2019-11-30 20:56:31 +0800

    Updates

---
+++ precache-manifest.2395759185f87bb2c4038b1eaff2f7f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9457b80eeba3fb706a2d43d2372002e",
+    "url": "/intersection-observer-api/index.html"
+  },
+  {
+    "revision": "c130a85b8982e510bbc5",
+    "url": "/intersection-observer-api/static/css/main.ca3f8d9a.chunk.css"
+  },
+  {
+    "revision": "e534a4fad0c40afd6178",
+    "url": "/intersection-observer-api/static/js/2.28f4a2e0.chunk.js"
+  },
+  {
+    "revision": "c130a85b8982e510bbc5",
+    "url": "/intersection-observer-api/static/js/main.68207f10.chunk.js"
+  },
+  {
+    "revision": "fc8c7ba6b89be5b3585d",
+    "url": "/intersection-observer-api/static/js/runtime-main.a9e45ff9.js"
+  }
+]);
