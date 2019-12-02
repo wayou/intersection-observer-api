1a9f779dc133cffe264d208e31f0b75a3330ea01
wayou <liuwayong@gmail.com>
2019-12-02 10:29:44 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
 
-const App: React.FC = () => {
+const IMAGE_API = "https://picsum.photos/200";
+const PAGE_SIZE = 10;
+const IMAGES_PAGED = new Array(PAGE_SIZE).fill(IMAGE_API);
+
+let loaderObserver: IntersectionObserver;
+let lazyLoadObserver: IntersectionObserver;
+let options = {
+  rootMargin: "0px",
+  threshold: 0
+};
+let page = 0;
+
+const App = () => {
+  const [images, setImages] = useState<string[]>(IMAGES_PAGED);
+
+  const infiniteCallback = useCallback<IntersectionObserverCallback>(
+    (_entries, _observer) => {
+      console.info(`loading page ${++page}`);
+      setImages(prev => [...prev, ...IMAGES_PAGED]);
+    },
+    []
+  );
+
+  const lazyLoadCallback = useCallback<IntersectionObserverCallback>(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // stop observer and load the image
+          const lazyImage = entry.target as HTMLImageElement;
+          console.log("lazy loading ", lazyImage);
+          lazyImage.classList.remove("empty");
+          lazyImage.src = lazyImage.dataset.src!;
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    []
+  );
+
+  //setup  infinite loading
+  useEffect(() => {
+    if (!loaderObserver) {
+      loaderObserver = new IntersectionObserver(infiniteCallback, options);
+    }
+    const target = document.querySelector("footer");
+    if (target) {
+      loaderObserver.observe(target);
+    }
+    return () => {
+      if (target) {
+        loaderObserver.unobserve(target);
+      }
+    };
+  }, [infiniteCallback]);
+
+  // setup lazy loading
+  useEffect(() => {
+    if (!lazyLoadObserver) {
+      lazyLoadObserver = new IntersectionObserver(lazyLoadCallback, options);
+    }
+    const imgs = document.querySelectorAll("img.empty");
+    if (imgs) {
+      imgs.forEach(img => {
+        lazyLoadObserver.observe(img);
+      });
+    }
+    return () => {
+      if (imgs) {
+        imgs.forEach(img => {
+          lazyLoadObserver.unobserve(img);
+        });
+      }
+    };
+  }, [lazyLoadCallback, images]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-wrap">
+        {images.map((image, index) => (
+          <img className="empty" key={index} data-src={image + `?f=${index}`} />
+        ))}
+      </div>
+      <footer>loading more ....</footer>
     </div>
   );
-}
+};
 
 export default App;
